9301f84efc0553c6bf363c5c07128620ee3e6ddf
IAM BARROSO DA COSTA <iam.barroso@sonda.com>
2023-06-27 17:27:12 -0400

    tratativa de exceptions ao tentar logar

--- src/main/java/app/netlify/iambarroso/escolaapi/service/AuthService.java
+++ src/main/java/app/netlify/iambarroso/escolaapi/service/AuthService.java
@@ -1,23 +1,20 @@
 package app.netlify.iambarroso.escolaapi.service;
 
-import app.netlify.iambarroso.escolaapi.config.EncoderConfig;
+import app.netlify.iambarroso.escolaapi.exceptions.AuthException;
+import app.netlify.iambarroso.escolaapi.rest.dto.LoginResponseDto;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
 import java.util.Date;
 
 @Service
@@ -25,32 +22,34 @@ import java.util.Date;
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
-    private final CustomUserDetailsService customUserDetailsService;
-    private final EncoderConfig encoderConfig;
 
     @Value("${jwt.secret}")
     private String jwtSecret;
     @Value("${jwt.expiration}")
     private Long jwtExpiration;
 
-    public String login(String username, String password) throws AuthenticationException {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(username, password));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+    public LoginResponseDto login(String username, String password) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-        if (!encoderConfig.passwordEncoder().matches(password, userDetails.getPassword())) {
-            throw new BadCredentialsException("Invalid username or password");
+            Date now = new Date();
+            Date expiryDate = new Date(now.getTime() + jwtExpiration);
+
+            return new LoginResponseDto(
+                    Jwts.builder()
+                    .setSubject(userDetails.getUsername())
+                    .setIssuedAt(now)
+                    .setExpiration(expiryDate)
+                    .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                    .compact());
+        } catch (InternalAuthenticationServiceException e) {
+            throw new AuthException(e.getMessage());
+        } catch (BadCredentialsException e) {
+            throw new AuthException("Credenciais inválidas, verifique login/senha e tente novamente");
         }
-        Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + jwtExpiration);
-
-        return Jwts.builder()
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(now)
-                .setExpiration(expiryDate)
-                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
-                .compact();
     }
 
 }
